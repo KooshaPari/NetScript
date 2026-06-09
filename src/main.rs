c5150dabefaa88af78faedae64f20b10e23ccc51
Forge <kooshapari@gmail.com>
2026-06-08 19:40:51 -0700

    Use App::run_once() instead of bypassing it in src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,8 @@ fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() > 1 {
         let input = &args[1];
-        let _app = App::new();
-        let adapter = netscript::CliAdapter::new();
-        let tokens = adapter.run_once(input);
+        let app = App::new();
+        let tokens = app.run_once(input);
         for token in tokens {
             println!("{:?}", token);
         }
